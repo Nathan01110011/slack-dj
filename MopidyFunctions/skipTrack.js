dd46b337657021cc6ca2e9e0a7278e9b554ec71d
Nathan <norr@deloitte.co.uk>
2020-02-17 11:27:14 +0000

    Refactor of skip track

--- MopidyFunctions/skipTrack.js
+++ MopidyFunctions/skipTrack.js
@@ -1,25 +1,19 @@
 var currentSong;
 var skipCount = 0;
 var users = [];
-var difference=0
 
-async function skipTrack(mopidy, options, user) {
+async function skipTrack(mopidy, options, user, command) {
   await resetVarsCheck(mopidy).catch();
   const userCheck = multipleVoteCheck(user);
   if (userCheck !== undefined) {
     return userCheck;
   }
-  if (command === "keep") {
-    difference = -1;
-  } else {
-    difference = 1;
-  }
-  return voteCountCheck(mopidy, difference);
+  return voteCountCheck(mopidy, command);
 }
 
 function multipleVoteCheck(user) {
   if (users.includes(user)) {
-    return "Sorry mate this is a democracy, not a dictatorship.";
+    return "Only one vote allowed per song I'm afraid.";
   } else {
     users.push(user);
   }
@@ -27,31 +21,24 @@ function multipleVoteCheck(user) {
 
 async function resetVarsCheck(mopidy) {
   const track = await mopidy.playback.getCurrentTrack();
-
   if (currentSong === undefined) {
     currentSong === (await mopidy.playback.getCurrentTrack());
   } else if (currentSong.name !== track.name) {
-    currentSong = undefined;
+    currentSong = await mopidy.playback.getCurrentTrack();
     skipCount = 0;
     users = [];
   }
 }
 
-async function voteCountCheck(mopidy) {
-  if (skipCount === 0) {
-    skipCount + difference;
-    currentSong = await mopidy.playback.getCurrentTrack();
-    const count = 3 - skipCount;
-    return "" + count + " more skips needed.";
-  } else if (skipCount < 2) {
-    skipCount + difference;
-    const count = 3 - skipCount;
-    return "" + count + " more skips needed.";
-  } else {
+async function voteCountCheck(mopidy, command) {
+  command === "skip" ? skipCount++ : skipCount--;
+  if (skipCount > 2) {
     mopidy.playback.next();
     skipCount = 0;
+    users = [];
     return "Okay then, I hope that wasn't an absolute *_tune and a half!_*";
   }
+  return `Skip count is at ${skipCount}. `;
 }
 
 module.exports.skipTrack = skipTrack;
