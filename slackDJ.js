f6777bbd2267a26265bef6a7711213dc484036b0
Nathan <norr@deloitte.co.uk>
2020-02-27 15:17:17 +0000

    adjusting how inital boot and handling server going offline

--- slackDJ.js
+++ slackDJ.js
@@ -5,18 +5,26 @@ const Mopidy = require(`mopidy`);
 
 const token = process.env.SLACK_TOKEN;
 const conversationId = process.env.SLACK_CHANNEL;
+var offline = true;
+var offlineMessageSent = false;
 
 // Creating RTM connection to Slack
 const rtm = new RTMClient(token);
 rtm.start().catch(console.error);
 rtm.on(`hello`, async () => {
-  await rtm.sendMessage(`Just getting ready!`, conversationId);
+  await rtm.sendMessage(`Hello everyone!`, conversationId);
   mopidy.connect();
 });
 rtm.on(`message`, event => {
   console.log(event);
-  if (event.subtype === undefined && event.channel === conversationId) {
+  if (
+    event.subtype === undefined &&
+    event.channel === conversationId &&
+    offline === false
+  ) {
     parseMessage(mopidy, rtm, event);
+  } else if (offline === true) {
+    rtm.sendMessage(`Music server not up yet!`, conversationId);
   }
 });
 
@@ -26,12 +34,20 @@ const mopidy = new Mopidy({
   webSocketUrl: `ws://localhost:6680/mopidy/ws`,
   backoffDelayMax: 10000
 });
-
 mopidy.on(`state`, console.log);
 mopidy.on(`event`, console.log);
 mopidy.on(`state:online`, () => {
   rtm.sendMessage(`Throw me some tunes! I'm ready to go!`, conversationId);
   mopidy.tracklist.setConsume({ value: true });
+  offlineMessageSent = false;
+  offline = false;
+});
+mopidy.on(`state:offline`, () => {
+  if (offlineMessageSent === false) {
+    rtm.sendMessage(`Give me a second, music server offline.`, conversationId);
+    offlineMessageSent = true;
+    offline = true;
+  }
 });
 
 nodeCleanup(function(exitCode, signal) {
